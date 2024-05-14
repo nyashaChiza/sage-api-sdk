7b9253d7b7c328ebbb05e8ad49814d04bda88f30
NyashaChiza <nchizampeni@gmail.com>
2024-05-14 09:32:46 +0200

    package init

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sage sdk',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Petalm Africa',
+    author_email='nyashac@petalmafrica.com',
+    description='Description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Petalm-Africa/sage-sdk.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
