547d574d74640a3178644ab2bde57112fea14c35
NyashaChiza <nchizampeni@gmail.com>
2024-05-14 10:10:11 +0200

    updated requirements

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,6 @@
 from test.test_connection import *
-from test.test_data_fetch import *
-from test.test_data_fetch import *
-from test.test_data_post import *
-from test.test_data_process import *
-from test.test_data_validation import *
+# from test.test_data_fetch import *
+# from test.test_data_fetch import *
+# from test.test_data_post import *
+# from test.test_data_process import *
+# from test.test_data_validation import *
